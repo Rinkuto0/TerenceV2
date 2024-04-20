483899137d1f777211c1bedb212c1a45323ae8b4
chidoka <98595203+Rinkuto0@users.noreply.github.com>
2024-04-20 10:44:40 +0300

    Update nowplaying.js

--- commands/music/nowplaying.js
+++ commands/music/nowplaying.js
@@ -27,7 +27,7 @@ const trackDuration = timestamp.progress == 'Forever' ? 'Endless (Live)' : track
         embed.setDescription(`Audio **%${queue.volume}**\nDuration **${trackDuration}**\nURL: ${track.url}\nLoop Mode **${methods[queue.repeatMode]}**\n${track. requestedBy}`);
 
         embed.setTimestamp();
-        embed.setFooter({ text: 'by Umut Bayraktar ❤️', iconURL: message.author.avatarURL({ dynamic: true }) });
+        embed.setFooter({ text: 'by chidoka', iconURL: message.author.avatarURL({ dynamic: true }) });
 
         const saveButton = new MessageButton();
 
